e01a8bbcbc3a5a5a664075ddbb8d33a0eed1d577
wilmer <wilmer7522@gmail.com>
2024-09-20 13:01:24 -0500

    feat :sparkles: company page done

--- js/modules/company.js
+++ js/modules/company.js
@@ -90,7 +90,71 @@ customElements.define("mi-nav2", ceo)
 
 
 
-/*class centro extends HTMLElement{
+class arriba extends HTMLElement{
+    constructor(){
+        super();
+        fetch(`https://api.spacexdata.com/v4/rockets`)
+    .then(res => res.json())
+    .then(data => {
+
+        
+console.log(data[0].flickr_images[1]);
+
+            
+            
+
+            this.innerHTML = 
+                `
+                        
+                    <article id="section__information__1" class="section__information__1">
+                        <mi-div class="load" style="height: 150px;">
+                            <marquee behavior="" direction="" scrollamount="20"  >
+
+                                
+                                
+
+                                <img src="${data[1].flickr_images[0]}"/>
+                                <img src="${data[1].flickr_images[1]}"/>
+                                <img src="${data[1].flickr_images[2]}"/>
+                                <img src="${data[1].flickr_images[3]}"/>
+                                <img src="${data[1].flickr_images[4]}"/>
+
+                                <img src="${data[2].flickr_images[0]}"/>
+                                <img src="${data[2].flickr_images[1]}"/>
+                                <img src="${data[2].flickr_images[2]}"/>
+                                <img src="${data[2].flickr_images[3]}"/>
+                                
+                                <img src="${data[3].flickr_images[0]}"/>
+                                <img src="${data[3].flickr_images[1]}"/>
+                                <img src="${data[3].flickr_images[2]}"/>
+                                <img src="${data[3].flickr_images[3]}"/>
+                                <img src="${data[3].flickr_images[4]}"/>
+                                
+                               
+                            </marquee>
+                        </mi-div>
+                    </article>
+                            
+                `
+
+            
+           
+
+            
+            
+        
+         
+    })
+
+    }
+}
+// <marquee behavior="" direction="" scrollamount="30"  ></marquee>
+
+customElements.define("mi-top", arriba)
+
+
+
+class centro extends HTMLElement{
     constructor(){
         super();
 fetch(`https://api.spacexdata.com/v4/company`)
@@ -101,8 +165,33 @@ fetch(`https://api.spacexdata.com/v4/company`)
     this.innerHTML = 
     `
 
-    `
+    
+     <div class="section__information__2">
 
+     
+                    <div>
+                        <div class="load" style="height: 150px;">2</div>
+                    </div>
+                    <div id="information__table__1" class="information__table__1">
+                        <div class="load" style="height: 160px;"><a href="${data.links.twitter}" class="twitter" >Twitter SpaceX    <img src="storage/img/icons/rocket_mini.svg"></a></div>
+                    </div>
+                </div>
+                <div id="section__image" class="section__image">
+                    <div class="load" style="height: 350px"><iframe src="${data.links.website}" width="100%" height="100%"></iframe></div>
+                </div>
+                <div class="section__information__3">
+                    <div>
+                        <div class="load" style="height: 150px;">5</div>
+                    </div>
+                    <div id="information__table__2" class="information__table__2">
+                        <div class="load" style="height: 160px;"><a href="${data.links.elon_twitter}" class="twitter" >Elon Twitter    <img src="storage/img/icons/rocket_mini.svg"></a></div>
+                    </div>
+                </div>
+
+    `
+    })
     }
-}*/
+}
+
+customElements.define("mi-article", centro)
         

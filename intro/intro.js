2cd0f406dd12657a3bc496e49ced7a0bea9162e6
JesusBustamante <jesusgustamante08@gmail.com>
2024-09-26 04:08:13 -0500

    feat: :sparkles: media query of intro

--- intro/intro.js
+++ intro/intro.js
@@ -1,3 +1,7 @@
 document.getElementById('btn').addEventListener('click', () => {
     window.location.href = '../Rocket/Rocket.html';
 })
+
+document.getElementById('boton').addEventListener('click', () => {
+    window.location.href = '../Rocket/Rocket.html';
+})
